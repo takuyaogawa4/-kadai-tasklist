c504acedc033cb0a9aa458432ba532a414455410
Shind <Shind@tekeya>
2023-10-30 15:42:39 +0900

    UpdateServlet Dispatcherリンク修正

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -47,7 +47,7 @@ public class UpdateServlet extends HttpServlet {
                 request.setAttribute("task", t);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/messages/edit.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/views/tasks/edit.jsp");
                 rd.forward(request, response);
             } else {
 
